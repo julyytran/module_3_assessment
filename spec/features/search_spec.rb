8f3e6209eee1ad9a1bf6a7484ddc7cf0a461e712
July Tran <julyytran@gmail.com>
2016-04-26 12:56:09 -0600

    tried and failed to figure out why returning 10 instead of 15

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,11 +2,12 @@ RSpec.describe "user can search by keyword" do
   it "returns results for search term 'sennheiser'" do
     visit root_path
 
+    VCR.use_cassette 'best_buy_service#get_products' do
     fill_in :q, :with => "sennheiser"
     click_on "Search"
 
-    expect(current_path).to eq "/search"
-
+      expect(current_path).to eq "/search"
+    end
     # And I should see exactly 15 results
     # And each result should contain sku, name, customer average review, short description, sale price, and image ONLY
   end
