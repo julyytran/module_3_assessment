a204680ba1d6ab290ddf2ed11445fb6f11bf18a7
July Tran <julyytran@gmail.com>
2016-04-26 09:18:06 -0600

    wrote test for items index api and created route for it

---
+++ spec/requests/api/v1/items_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe "it returns all the items" do
+  it "shows a list of items and their attributes " do
+    get "/api/v1/items"
+
+    expect(response.status).to eq 200
+
+    json_body = JSON.parse(response.body)
+
+    expect(json_body[0]).to eq ({
+      "name" => "Gorgeous Iron Bottle",
+      "description" => "Quo animi vel non ea assumenda. Reprehenderit a perspiciatis numquam. Repellendus quia sit rerum.",
+      "image_url" => "http://robohash.org/0.png?set=set2&bgset=bg1&size=200x200"
+    })
+  end
+end
+
+# When I send a GET request to `/api/v1/items`
+# I receive a 200 JSON response containing all items
+# And each item has a name, description, and image_url but not the created_at or updated_at
